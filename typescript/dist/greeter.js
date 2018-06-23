c0cc4e95cdae8c035c3d726c15840ebc0a94fb1e
chentn <chentn@fenbi.com>
2018-06-23 15:46:33 +0800

    DEL: dist from git

--- typescript/dist/greeter.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greeter(person) {
-    console.log("Hello, " + person.firstName + " " + person.lastName);
-}
-exports.greeter = greeter;
--- typescript/dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var greeter_1 = require("./greeter");
-function hello(compiler) {
-    console.log(greeter_1.greeter({ firstName: "tn", lastName: "chen" }));
-}
-hello("TypeScript");
