d1798387e472de6744c9809050c4bb24912a924b
chentn <chentn@fenbi.com>
2021-12-08 18:42:15 +0800

    MOD: opt with iterator

--- rust/minigrep/src/lib.rs
+++ rust/minigrep/src/lib.rs
@@ -31,26 +31,13 @@ mod tests {
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = &query.to_lowercase();
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(query) {
-            result.push(line);
-        }
-    }
 
-    result
+    let query = &query.to_lowercase();
+    contents.lines().filter(|line| line.to_lowercase().contains(query)).collect()
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-    result
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
@@ -78,14 +65,23 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("no enough parameters!");
-        }
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+
+        args.next();
+
+        let query = match args.next() {
+            Some(s) => s,
+            None => return Err("Didn't get a query string")
+        };
+        let filename = match args.next() {
+            Some(s) => s,
+            None => return Err("Didn't get a filename string")
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        println!("query: {}, filename: {}, case_sensitive: {}", query, filename, case_sensitive);
 
         Ok(Config { query, filename, case_sensitive})
     }
