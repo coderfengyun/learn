ca9016cf7c4eefef03180722ba9de2095ae73a72
chentn <chentn@fenbi.com>
2021-12-15 12:01:56 +0800

    ADD: encoding objects into typs

--- rust/object_oriented/src/lib.rs
+++ rust/object_oriented/src/lib.rs
@@ -4,7 +4,6 @@ pub struct Post {
 }
 
 pub trait State {
-
     fn request_review(self: Box<Self>) -> Box<dyn State>;
 
     fn approve(self: Box<Self>) -> Box<dyn State>;
@@ -23,9 +22,8 @@ pub trait State {
 pub struct Draft {}
 
 impl State for Draft {
-
     fn request_review(self: Box<Self>) -> Box<dyn State> {
-        Box::new(PendingReview { approved_times: 0})
+        Box::new(PendingReview { approved_times: 0 })
     }
 
     fn approve(self: Box<Self>) -> Box<dyn State> {
@@ -46,16 +44,15 @@ pub struct PendingReview {
 }
 
 impl State for PendingReview {
-
     fn request_review(self: Box<Self>) -> Box<dyn State> {
-        Box::new(PendingReview { approved_times: 0})
+        Box::new(PendingReview { approved_times: 0 })
     }
 
     fn approve(mut self: Box<Self>) -> Box<dyn State> {
         self.approved_times += 1;
 
         if self.approved_times == 2 {
-           return Box::new(Published{});
+            return Box::new(Published {});
         }
 
         self
@@ -69,7 +66,6 @@ impl State for PendingReview {
 pub struct Published {}
 
 impl State for Published {
-
     fn request_review(self: Box<Self>) -> Box<dyn State> {
         self
     }
@@ -88,7 +84,6 @@ impl State for Published {
 }
 
 impl Post {
-
     pub fn new() -> Post {
         Post {
             state: Some(Box::new(Draft {})),
@@ -97,7 +92,6 @@ impl Post {
     }
 
     pub fn add_text(&mut self, text: &str) {
-
         if self.state.as_ref().unwrap().can_add_text() {
             self.content = String::from(text);
         }
@@ -122,6 +116,88 @@ impl Post {
     pub fn request_review(&mut self) {
         if let Some(s) = self.state.take() {
             self.state = Some(s.request_review());
+        }
+    }
+}
+
+pub mod tests {
+    use crate::Post;
+
+    #[test]
+    fn test() {
+        let mut post = Post::new();
+
+        post.add_text("I ate a salad for lunch today");
+        assert_eq!("", post.content());
+
+        post.request_review();
+        assert_eq!("", post.content());
+
+        post.approve();
+        assert_eq!("", post.content());
+
+        post.approve();
+        assert_eq!("I ate a salad for lunch today", post.content());
+    }
+}
+
+pub mod encode_types {
+
+    pub struct Post {
+        content: String,
+    }
+
+    pub struct DraftPost {
+        content: String,
+    }
+
+    pub struct PendingReviewPost {
+        content: String,
+    }
+
+    impl Post {
+        pub fn new() -> DraftPost {
+            DraftPost {
+                content: String::from(""),
+            }
+        }
+
+        pub fn content(&self) -> &str {
+            self.content.as_str()
+        }
+    }
+
+    impl DraftPost {
+        pub fn add_text(&mut self, text: &str) {
+            self.content.push_str(text);
+        }
+
+        pub fn request_review(self) -> PendingReviewPost {
+            PendingReviewPost {
+                content: self.content,
+            }
+        }
+    }
+
+    impl PendingReviewPost {
+        pub fn approve(self) -> Post {
+            Post {
+                content: self.content,
+            }
+        }
+    }
+
+    pub mod tests {
+        use crate::encode_types::Post;
+
+        #[test]
+        fn should_got_content() {
+            let mut draft_post = Post::new();
+            draft_post.add_text("I ate a salad for launch");
+
+            let post = draft_post.request_review().approve();
+
+            assert_eq!("I ate a salad for launch", post.content());
         }
     }
 }
