ccc010749fcb95299d64284b4b9c5b29e19cb0a7
chentn <chentn@fenbi.com>
2021-12-24 16:41:42 +0800

    ADD: fulfill web_server with my thread-pool

--- rust/web_server/src/bin/main.rs
+++ rust/web_server/src/bin/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let tcp_listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in tcp_listener.incoming() {
+    for stream in tcp_listener.incoming().take(2) {
         let stream = stream.unwrap();
         pool.execute(|| handle_connection(stream));
     }
--- rust/web_server/src/lib.rs
+++ rust/web_server/src/lib.rs
@@ -1,3 +1,4 @@
+use crate::Message::{NewJob, Terminate};
 use std::sync::mpsc::{Receiver, Sender};
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
@@ -5,7 +6,12 @@ use std::thread::JoinHandle;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: Sender<Job>,
+    sender: Sender<Message>,
+}
+
+pub enum Message {
+    NewJob(Job),
+    Terminate,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -15,11 +21,9 @@ impl ThreadPool {
     where
         F: 'static + FnOnce() -> () + Send,
     {
-        self.sender.send(Box::new(f));
+        self.sender.send(Message::NewJob(Box::new(f))).unwrap();
     }
-}
 
-impl ThreadPool {
     ///Create a new ThreadPool
     ///
     /// The thread_num is the number of the thread in pool
@@ -30,33 +34,59 @@ impl ThreadPool {
     pub fn new(thread_num: usize) -> ThreadPool {
         assert!(thread_num > 0, "thread_num must L.T. 0");
         let mut workers: Vec<Worker> = Vec::with_capacity(thread_num);
-        let (sender, receiver): (Sender<Job>, Receiver<Job>) = mpsc::channel();
+        let (sender, receiver): (Sender<Message>, Receiver<Message>) = mpsc::channel();
         let receiver = Arc::new(Mutex::new(receiver));
 
         for idx in 0..thread_num {
             workers.push(Worker::new(idx, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender }
+        ThreadPool {
+            workers,
+            sender,
+        }
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+
+        println!("Sending Terminate message to all workers");
+        for _worker in &mut self.workers {
+            self.sender.send(Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers");
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(join_handler) = worker.thread.take() {
+                join_handler.join().unwrap();
+            }
+        }
     }
 }
 
 pub struct Worker {
     id: usize,
-    thread: JoinHandle<()>,
+    thread: Option<JoinHandle<()>>,
 }
 
 impl Worker {
-    pub fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<Receiver<Message>>>) -> Worker {
         Worker {
             id,
-            thread: thread::spawn(move || {
-                loop {
-                    let job = receiver.lock().unwrap().recv().unwrap();
-                    println!("worker {} get a job; executing", id);
-                    job();
+            thread: Some(thread::spawn(move || loop {
+                match receiver.lock().unwrap().recv().unwrap() {
+                    NewJob(f) => {
+                        println!("worker {} get a job; executing", id);
+                        f();
+                    }
+                    Terminate => {
+                        println!("worker {} get a terminate message; terminating...", id);
+                        break;
+                    }
                 }
-            }),
+            })),
         }
     }
 }
